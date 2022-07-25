bdf768d3850bf2594ce53b7927444a4dd2278eab
chutilin0616ncyu <chutilin@mail.ncyu.edu.tw>
2022-07-25 21:22:19 +0800

    6th commit- including checkstyle

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,10 +1,16 @@
-public class App {
+/**
+ * Javadoc.
+ */
 
+public class App {
+  /**
+   * Javadoc.
+   */
   public static void main(String[] args) {
     GradeConverter converter = new GradeConverter();
 
-    for(int i=0; i< 105; i++) {
-      System.out.println(i+";"+converter.convert(i));
+    for (int i = 0; i < 105; i++) {
+      System.out.println(i + ";" + converter.convert(i));
     }
   }
 }
